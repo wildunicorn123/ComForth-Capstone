f6d25ea92cba90f7ff613a961c1d589778e96913
wildunicorn123 <amberkayeafr6901@gmail.com>
2023-09-07 16:18:32 +0200

    front

---
+++ frontend/comforth/src/components/navbarComp.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarToggleExternalContent"
+      aria-controls="navbarToggleExternalContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+  </div>
+</nav>
+</template>
+<style scoped>
+
+</style>
